c4c08a18b88ffe11942fae26c38ed93ab42a24cc
ricardo-ervilha <ricardoervilha456@gmail.com>
2024-09-21 18:37:33 -0300

    Parte do victorvalho feita. PS: Falta atualizar a data lá também durante a conversão...

--- pokemon/app/Http/Controllers/PokemonController.php
+++ pokemon/app/Http/Controllers/PokemonController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Pokemon;
+use App\Models\PokemonCapturado;
+use App\Models\PokemonSelvagem;
+use Illuminate\Support\Facades\DB;
 
 class PokemonController extends Controller
 {
@@ -19,8 +22,27 @@ class PokemonController extends Controller
     }
 
     public function initial_pokemons(Request $request){
+
+        //array contendo os pokémons selecionados na tela inicial
         $pokemons = $request->input('selected_pokemons');
         $array = json_decode($pokemons, true);
-        dd($array);
+        
+        //para cada pokémon selvagem, converto em pokémon capturado
+        foreach($array as $pokemon_name){
+            $pokemonSelvagem = DB::table('pokemon_selvagem')
+                             ->join('pokemon', 'pokemon_selvagem.id_pokemon', '=', 'pokemon.id')
+                             ->where('nome', '=', $pokemon_name)->first();
+            
+            PokemonCapturado::create([
+                'id_pokemon' => $pokemonSelvagem->id_pokemon,
+                'id_treinador' => auth()->guard('player')->user()->codigo_treinador,
+            ]);
+
+            DB::table('pokemon_selvagem')
+                ->where('id_pokemon', $pokemonSelvagem->id_pokemon)
+                ->delete();
+
+            }
+        return redirect()->route('dashboard');
     }
 }
